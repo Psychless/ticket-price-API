cc5532f907b3b642943cf5aedb8dbe6c56b04817
Kristaps Baumanis <kristaps.baumanis@autentica.lv>
2021-05-06 11:08:26 +0300

    Initial commit

---
+++ src/main/java/com/psychless/ticketprice/TicketpriceApplication.java
@@ -0,0 +1,13 @@
+package com.psychless.ticketprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketpriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketpriceApplication.class, args);
+	}
+
+}
